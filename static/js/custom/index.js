45cab306450710a2e36063c4b8156c4683321b41
Kyle Lin <klin12@stuy.edu>
2017-12-22 10:21:16 -0500

    Added a neat breathing animation for the index logo

--- static/js/custom/index.js
+++ static/js/custom/index.js
@@ -12,12 +12,7 @@ var wait = function(ms) {
 
 var addCount = function(e) {
     counter++;
-    console.log(img);
-    e.target.style.width = "500px";
     display_count.innerHTML = "Easter Egg! You've clicked this logo " + boldCounter() + " times!";
-    wait(250);
-    console.log(img);
-    e.target.style.width = "720px";
 };
 
 var boldCounter = function(e) {
